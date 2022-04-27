2ff2d4784a7bf3f6d44ee8907803b704ba25583d
J. S. Oishi <jsoishi@gmail.com>
2022-04-26 21:58:04 -0400

    Working HWI DNS script.

--- python/hwi_dns_2d.py
+++ python/hwi_dns_2d.py
@@ -71,7 +71,7 @@ ez['g'][1] = 1
 
 integ = lambda A: d3.Integrate(d3.Integrate(A, 'x'), 'z')
 
-lift_basis = zbasis.clone_with(a=1/2, b=1/2) # First derivative basis
+lift_basis = zbasis.derivative_basis(1) # First derivative basis
 lift = lambda A: d3.Lift(A, lift_basis, -1) # First-order reduction
 grad_u = d3.grad(u) + ez*lift(tau_u1)
 grad_rho = d3.grad(rho) + ez*lift(tau_rho1) 
@@ -79,8 +79,8 @@ grad_rho = d3.grad(rho) + ez*lift(tau_rho1)
 # Problem
 problem = d3.IVP([u, rho, p, tau_rho1, tau_rho2, tau_u1, tau_u2, tau_p], namespace=locals())
 problem.add_equation("trace(grad_u) + tau_p= 0")
-problem.add_equation("dt(u) + grad(p) - div(grad_u)/Re + Rib*rho*ez + lift(tau_u2) = - dot(u,grad(u))")
-problem.add_equation("dt(rho) - div(grad_rho)/(Re*Pr) + lift(tau_rho2) = - dot(u,grad(rho))")
+problem.add_equation("dt(u) + grad(p) - div(grad_u)/Re + Rib*rho*ez + lift(tau_u2) = -u@grad(u)")
+problem.add_equation("dt(rho) - div(grad_rho)/(Re*Pr) + lift(tau_rho2) = -u@grad(rho)")
 problem.add_equation("integ(p) = 0") # Pressure gauge
 
 # boundary conditions
@@ -99,25 +99,18 @@ solver.stop_sim_time = stop_sim_time
 # Initial conditions
 # Background shear
 u['g'][0] = np.tanh(z)
-# Background density stratification
-# if rho_ampl != 0:
-#     rho.fill_random('g', seed=42, distribution='normal', scale=rho_ampl)
-#rho['g'] *= np.exp(-z**2)
 rho['g'] = (1-np.tanh(Aspect*z)) 
 
-
-
-# Add small vertical velocity perturbations localized to the shear layers
+# Add small velocity perturbations localized to the shear layers
+# use vector potential to ensure div(u) = 0
 A = dist.Field(name='A', bases=(xbasis,zbasis))
 A.fill_random('g', seed=42, distribution='normal')
 A.low_pass_filter(scales=(0.2, 0.2, 0.2))
-A['g'] *= 1 - (2*z/Lz)**2 # Damp noise at walls
+A['g'] *= (1 - (2*z/Lz)**2) *np.exp(-z**2) # Damp noise at walls
 
 up = d3.skew(d3.grad(A)).evaluate()
 up.change_scales(1)
 u['g'] += 1e-3*up['g'] 
-
-#u['g'][1] += 0.1 * np.sin(2*np.pi*x/Lx) * np.exp(-z**2)
 
 # Analysis
 if dist.comm.rank == 0:
@@ -125,17 +118,19 @@ if dist.comm.rank == 0:
         datadir.mkdir(parents=True)
     
 
-snapshots = solver.evaluator.add_file_handler(datadir/'snapshots', sim_dt=0.05, max_writes=10)
+snapshots = solver.evaluator.add_file_handler(datadir/'snapshots', sim_dt=1., max_writes=10)
 snapshots.add_task(rho)
 snapshots.add_task(u)
 
 # CFL
-CFL = d3.CFL(solver, initial_dt=max_timestep, cadence=10, safety=0.3, threshold=0.1, max_dt=max_timestep)
+CFL = d3.CFL(solver, initial_dt=1e-3, cadence=10, safety=0.2, threshold=0.05, max_change=1.5, min_change=0.5, max_dt=max_timestep)
 CFL.add_velocity(u)
-
+if Aspect != 0:
+    CFL.add_frequency((Rib*((d3.grad(rho)*ez)**2)**0.5)**0.5)
 # Flow properties
 flow = d3.GlobalFlowProperty(solver, cadence=10)
 flow.add_property(d3.dot(u,ez)**2, name='w2')
+flow.add_property(d3.div(u), name='divu')
 
 # Main loop
 try:
@@ -145,7 +140,8 @@ try:
         solver.step(timestep)
         if (solver.iteration-1) % 10 == 0:
             max_w = np.sqrt(flow.max('w2'))
-            logger.info('Iteration=%i, Time=%e, dt=%e, max(w)=%f' %(solver.iteration, solver.sim_time, timestep, max_w))
+            max_divu = flow.max('divu')
+            logger.info('Iteration=%i, Time=%e, dt=%e, max(w)=%f, max(div(u)=%e' %(solver.iteration, solver.sim_time, timestep, max_w, max_divu))
 except:
     logger.error('Exception raised, triggering end of main loop.')
     raise
